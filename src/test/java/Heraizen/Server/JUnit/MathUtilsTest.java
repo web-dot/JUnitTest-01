33c38779bda51f40e7a79cc3588e22dfa6ae8ba2
sudipto <sudipto.c@heraizen.com>
2022-01-09 23:34:44 +0530

    junit test 01

---
+++ src/test/java/Heraizen/Server/JUnit/MathUtilsTest.java
@@ -0,0 +1,36 @@
+package Heraizen.Server.JUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+    MathUtils mathUtils;
+    
+    @BeforeAll
+    void init() {
+        mathUtils = new MathUtils();
+    }
+    
+    
+    @Test
+    void testAdd() {
+        int expected = 2;
+        int actual = mathUtils.add(1, 1);
+        assertEquals(expected, actual, "The add method should add two numbers"); //asserting that expected and actual are the same
+    }
+    
+    @Test
+    void testDivide() {
+        assertThrows(NullPointerException.class, () -> mathUtils.divide(1, 0), "divide by 0 should throw");
+    }
+    
+    @Test
+    void testComputeCircleArea() {
+        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return circle area of the radius");
+    }
+    
+    
+}
